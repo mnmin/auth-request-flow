8d6cf7da2a8753e0ee91e7deebf340e36503457c
Michele M <mnmin@gmail.com>
2022-09-05 16:29:22 +0100

    Login

--- src/router.js
+++ src/router.js
@@ -14,11 +14,35 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    //console.log(req.body)
+
+    const { username, password, profile } = req.body
+    const { firstName, lastName, age} = req.body
+
+    const token = jwt.sign({ username: 'authguy', profile: {
+        firstName: 'Chris',
+        lastName: "Wolstenholme",
+        age: 43
+    } }, 'mypassword',)
+
+    res.json(token)
 
 });
 
 router.get('/profile', (req, res) => {
   
+    //console.log(req.headers)
+    //console.log(req.headers.authorization)
+
+    const newToken = req.headers.authorization
+
+    console.log(newToken)
+    // try {
+    //     const result = jwt.verify(token, newToken)
+    // } catch (e) {
+    //     return false
+    // }
+    // res.json(result)
 });
 
 
